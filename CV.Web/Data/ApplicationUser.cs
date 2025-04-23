62cfdb0d1ed04af819d24023e5d85d3736e4e75f
Lucato <lukwis95@gmail.com>
2025-04-23 19:49:48 +0200

    commmit blazor

---
+++ CV.Web/Data/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace CV.Web.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+}
+
